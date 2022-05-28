0ce1d032879fee86f06865ddef0c27caf604500d
hamedahmedSoftexsw <96015548+hamedahmedSoftexsw@users.noreply.github.com>
2022-05-28 23:15:49 +0200

    return connection

--- config/database.php
+++ config/database.php
@@ -21,7 +21,7 @@ class Database{
   
         try{
             $this->conn = new PDO("pgsql:host=" . $this->host . ";dbname=" . $this->db_name, $this->username, $this->password);
-            $this->conn->exec();
+            $this->conn->exec("set names utf8");
 
         }catch(Exception $exception){
             echo "Connection error: " . $exception->getMessage();
